70bf1420d02639299405bbb1d760c52c25750c2a
steventhanna <steventhanna@gmail.com>
2017-08-12 22:24:03 -0500

    link working

--- nba-predict.py
+++ nba-predict.py
@@ -1,4 +1,11 @@
+import requests
+import json
+
 def main():
+    data = {'name': 'jokic'}
+    r = requests.post('https://nba-api.therendersports.com/player/findFromName', json=data)
+    r = r.json()
+    print(r)
 
 if __name__ == '__main__':
     main()
